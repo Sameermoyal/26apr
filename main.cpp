3c1c788d8c648fd5f71a04327d9ea89173762724
Sameer  Moyal <sameerab827@gmail.com>
2024-04-26 12:46:50 +0530

    fbsdbk

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<string>fruit;
+    
+    for(auto &it:fruit){
+        cin>>it;
+    }
+    
+    for(auto it:fruit){
+        cout<<it<<" ";
+    }
+}
